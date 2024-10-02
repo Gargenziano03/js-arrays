41f25ee5a2f0d05eaac4ea1e8b209f5926e83494
Gargenziano03 <gargenziano03@gmail.com>
2024-10-02 16:19:01 +0200

    add fix

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,11 +10,11 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[4];
+const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(5, 1, "Patrick")
+teachers.splice(4, 1, "Patrick")
 console.log(teachers);
 
 
